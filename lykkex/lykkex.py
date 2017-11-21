009a546db77f04aa494de4c38bf80ed1aa8b7d81
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2017-11-22 00:08:58 +0100

    * added fetching order books

--- lykkex/lykkex.py
+++ lykkex/lykkex.py
@@ -2,8 +2,19 @@ import requests
 
 
 class LykkexConstants(object):
+    ORDER_BOOKS_RELATIVE_URL = "OrderBooks"
     BASE_URL = "https://hft-service-dev.lykkex.net/api/"
+    IS_ALIVE_RELATIVE_URL = "IsAlive"
 
 
 def is_alive():
-    return requests.get(LykkexConstants.BASE_URL + "IsAlive").json()
+    return requests.get(LykkexConstants.BASE_URL + LykkexConstants.IS_ALIVE_RELATIVE_URL).json()
+
+
+def get_order_books():
+    return requests.get(LykkexConstants.BASE_URL + LykkexConstants.ORDER_BOOKS_RELATIVE_URL).json()
+
+
+def get_order_book(asset_pair_id):
+    return requests.get(
+        LykkexConstants.BASE_URL + LykkexConstants.ORDER_BOOKS_RELATIVE_URL + '/' + asset_pair_id).json()
--- tests/test_lykkex.py
+++ tests/test_lykkex.py
@@ -2,11 +2,25 @@ from unittest import TestCase
 
 import vcr
 
-import lykkex
+import lykkex.lykkex as lykkex
 
 
 class TestLykkex(TestCase):
     @vcr.use_cassette('vcr_cassetes/is_alive.yml')
     def test_is_alive_returns_a_dict_with_a_field_for_possible_issues(self):
         response = lykkex.is_alive()
-        assert "IssueIndicators" in response.keys()
+        self.assertTrue("IssueIndicators" in response.keys())
+
+    @vcr.use_cassette('vcr_cassetes/order_books.yml')
+    def test_get_order_books_retrieves_a_list_of_order_books(self):
+        list_of_order_books = lykkex.get_order_books()
+        self.assertIsInstance(list_of_order_books, list)
+
+    @vcr.use_cassette('vcr_cassetes/order_book.yml')
+    def test_get_order_book_fetches_the_correct_order_book(self):
+        asset_pair_id = 'AUDUSD'
+        order_book = lykkex.get_order_book(asset_pair_id)
+        self.assertIsInstance(order_book, list)
+
+        for entry in order_book:
+            self.assertEqual(entry["AssetPair"], asset_pair_id)
