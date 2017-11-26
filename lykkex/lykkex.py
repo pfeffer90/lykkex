b14d18af03d242da08a9e56d65315ee8ead0230f
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2017-11-26 13:51:28 +0100

    * refactoring

--- lykkex/lykkex.py
+++ lykkex/lykkex.py
@@ -12,46 +12,49 @@ class LykkexConstants(object):
     WALLET_URL = 'https://hft-service-dev.lykkex.net/api/Wallets'
 
 
+def _get_header(api_key):
+    return {'api-key': api_key,
+            'Content-Type': 'application/json'}
+
+
 def is_alive():
-    return json.loads(urlopen(LykkexConstants.BASE_URL + LykkexConstants.IS_ALIVE_RELATIVE_URL).read().decode())
+    return json.loads(urlopen(LykkexConstants.BASE_URL + LykkexConstants.IS_ALIVE_RELATIVE_URL).read())
 
 
 def get_order_books():
-    return json.loads(urlopen(LykkexConstants.BASE_URL + LykkexConstants.ORDER_BOOKS_RELATIVE_URL).read().decode())
+    return json.loads(urlopen(LykkexConstants.BASE_URL + LykkexConstants.ORDER_BOOKS_RELATIVE_URL).read())
 
 
 def get_order_book(asset_pair_id):
     return json.loads(urlopen(
-        LykkexConstants.BASE_URL + LykkexConstants.ORDER_BOOKS_RELATIVE_URL + '/' + asset_pair_id).read().decode())
+        LykkexConstants.BASE_URL + LykkexConstants.ORDER_BOOKS_RELATIVE_URL + '/' + asset_pair_id).read())
 
 
-def get_balance(API_KEY):
-    return json.loads(urlopen(Request(LykkexConstants.WALLET_URL, headers={'api-key': API_KEY,
-                                                                           'Content-Type': 'application/json'})).read().decode())
+def get_balance(api_key):
+    return json.loads(urlopen(Request(LykkexConstants.WALLET_URL, headers=_get_header(api_key))).read())
 
 
-def get_pending_orders(API_KEY):
+def get_pending_orders(api_key):
     return json.loads(urlopen(Request(LykkexConstants.BASE_URL + 'Orders?status=InOrderBook',
-                                      headers={'api-key': API_KEY,
-                                               'Content-Type': 'application/json'})).read().decode())
+                                      headers=_get_header(api_key))).read())
 
 
-def send_market_order(API_KEY, asset_pair, asset, order_action='BUY', volume='0.1'):
+def get_order_status(api_key, order_id):
+    return json.loads(urlopen(Request(LykkexConstants.BASE_URL + 'Orders/' + order_id,
+                                      headers=_get_header(api_key))).read())
+
+
+def send_market_order(api_key, asset_pair, asset, order_action, volume):
     data = json.dumps(
         {"AssetPairId": asset_pair, "Asset": asset, "OrderAction": order_action,
          "Volume": volume}).encode("utf8")
     return json.loads(urlopen(Request(LykkexConstants.BASE_URL + 'Orders/market', data,
-                                      {'api-key': API_KEY, 'Content-Type': 'application/json'})).read())
+                                      headers=_get_header(api_key))).read())
 
 
-def send_limit_order(API_KEY, asset_pair, asset, price, order_action='BUY', volume='0.1'):
+def send_limit_order(api_key, asset_pair, asset, price, order_action, volume):
     data = json.dumps(
         {"AssetPairId": asset_pair, "Asset": asset, "OrderAction": order_action,
          "Volume": volume, "Price": price}).encode("utf8")
     return json.loads(urlopen(Request(LykkexConstants.BASE_URL + 'Orders/limit', data,
-                                      {'api-key': API_KEY, 'Content-Type': 'application/json'})).read())
-
-
-def control_limit_order(API_KEY, order_id):
-    return json.loads(urlopen(Request(LykkexConstants.BASE_URL + 'Orders/' + order_id,
-                                      headers={'api-key': API_KEY, 'Content-Type': 'application/json'})).read())
+                                      headers=_get_header(api_key))).read())
